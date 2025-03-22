382747ea7ef4b992a989550a75098c44b82e659b
pappu4903 <pappu4903@gmail.com>
2025-03-22 22:07:11 +0530

    "basic commit"

---
+++ playwright.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: './tests',
+  reporter: "html",
+  /* Run tests in files in parallel */
+  
+  use: {
+    browserName:"chromium"
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    // baseURL: 'http://127.0.0.1:3000',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    //trace: 'on-first-retry',
+  },
+
+  
+    
+
+});
